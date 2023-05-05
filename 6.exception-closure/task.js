9d79be7baafdb59e7afc701725a0b50564b5f7ee
ELETRAUD <122927068+ELETRAUD@users.noreply.github.com>
2023-05-05 16:55:25 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,51 +1,107 @@
 function parseCount(value) {
-  const parsedValue = Number.parseFloat(value);
-  if (isNaN(parsedValue)) {
-    throw new Error("Невалидное значение");
-  }
-  return parsedValue;
+ 
+const parsedValue = Number.parseFloat(value);
+ 
+ 
+ 
+if (Number.isNaN(parsedValue)) {
+ 
+throw new Error("Невалидное значение");
+ 
 }
-
+ 
+ 
+ 
+return parsedValue;
+ 
+}
+ 
+ 
+ 
 function validateCount(value) {
-  try {
-    return parseCount(value);
-  } catch (error) {
-    return error;
-  }
+ 
+try {
+ 
+return parseCount(value);
+ 
+} catch (error) {
+ 
+return error;
+ 
 }
-
-//Задача 2. Треугольник
-
+ 
+}
+ 
 class Triangle {
-  constructor(a, b, c) {
-    if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error("Треугольник с такими сторонами не существует");
-    }
-    this.a = a;
-    this.b = b;
-    this.c = c;
-  }
-
-  getPerimeter() {
-    return this.a + this.b + this.c;
-  }
-
-  getArea() {
-    const p = this.getPerimeter() / 2;
-    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-    return +area.toFixed(3);
-  }
+ 
+constructor(a, b, c) {
+ 
+if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+ 
+throw new Error("Треугольник с такими сторонами не существует");
+ 
 }
-
+ 
+ 
+ 
+this.a = a;
+ 
+this.b = b;
+ 
+this.c = c;
+ 
+}
+ 
+ 
+ 
+get perimeter() {
+ 
+return this.a + this.b + this.c;
+ 
+}
+ 
+ 
+ 
+get area() {
+ 
+const p = this.perimeter / 2;
+ 
+const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+ 
+return +area.toFixed(3);
+ 
+}
+ 
+}
+ 
+ 
+ 
 function getTriangle(a, b, c) {
-  try {
-    return new Triangle(a, b, c);
-  } catch (error) {
-    return {
-      getArea: () => "Ошибка! Треугольник не существует",
-      getPerimeter: () => "Ошибка! Треугольник не существует",
-    };
-  }
+ 
+try {
+ 
+return new Triangle(a, b, c);
+ 
+} catch (error) {
+ 
+return {
+ 
+get area() {
+ 
+return "Ошибка! Треугольник не существует";
+ 
+},
+ 
+get perimeter() {
+ 
+return "Ошибка! Треугольник не существует";
+ 
+}
+ 
+}
+ 
+}
+ 
 }
 
 
